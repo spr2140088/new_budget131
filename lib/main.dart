bc5048780c68d1fc6b205a1a28a638a415eb205e
木本晃規 <spr2140088@stu.o-hara.ac.jp>
2023-01-31 14:38:34 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  double month = 0;
+  double week = 0;
+  int day = 0;
+
+  int weeks = 4;
+  int days = 7;
+  String aaa = '';
+
+  TextEditingController moneyController = TextEditingController();
+
+   String weekCalc(double month) {
+    week = (month / 4);
+    return week.toString();
+  }
+
+  String dayCalc(int week, int days) {
+    day = (week / days) as int;
+    return day.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(month);
+    print(week);
+    print(day);
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70.50),
+        child: AppBar(
+          title: Text('今月の予算', style: TextStyle(fontSize: 25),),
+          centerTitle: true,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 60,),
+            Container(
+              width: double.infinity,
+              height: 80,
+              //color: Colors.cyanAccent,
+              child: Row(
+                children: <Widget>[
+                  SizedBox(width: 30,),
+                  Container(
+                    width: 210,
+                    height: 50,
+                    //color: Colors.red,
+                    child: TextField(
+                      controller: moneyController,
+                      maxLength: 7,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: const Color(0xFFf0f8ff),
+                        hintText: '予算を入力してください'
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 40,),
+                  Container(
+                    width: 70,
+                    height: 50,
+                    color: Colors.orange,
+                    child: ElevatedButton(
+                      child: const Text('設定', style: TextStyle(fontSize: 18),),
+                      onPressed: () {
+                        setState(() {
+                          month = double.parse(moneyController.text);
+                          aaa = weekCalc(month);
+                          dayCalc;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 40,),
+            Container(
+              width: double.infinity,
+              height: 40,
+              child: Text('使用金額', style: TextStyle(fontSize: 30),),
+            ),
+            SizedBox(height: 30,),
+            Container(
+              width: double.infinity,
+              height: 80,
+              decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.red,
+                      width: 10,
+                    ),
+                  )
+              ),
+              child: Row(
+                children: <Widget>[
+                  Text('今月', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 25,),
+                  Text('¥', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text('30,000', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text('/', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text('¥', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text(aaa, style: TextStyle(fontSize:30),),
+                ],
+              ),
+            ),
+            SizedBox(height: 40,),
+            Container(
+              width: double.infinity,
+              height: 80,
+              decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.blue,
+                      width: 10,
+                    ),
+                  )
+              ),
+              child: Row(
+                children: <Widget>[
+                  Text('今週', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 25,),
+                  Text('¥', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text('7,000', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text('/', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text('¥', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text("${week.toString()}", style: TextStyle(fontSize:30),),
+                ],
+              ),
+            ),
+            SizedBox(height: 40,),
+            Container(
+              width: double.infinity,
+              height: 80,
+              decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.green,
+                      width: 10,
+                    ),
+                  )
+              ),
+              child: Row(
+                children: <Widget>[
+                  Text('今日', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 25,),
+                  Text('¥', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text('2,000', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text('/', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text('¥', style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                  Text("${day}", style: TextStyle(fontSize:30),),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {},
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
